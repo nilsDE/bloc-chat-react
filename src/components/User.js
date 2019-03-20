f583f9f08f434f38e9191a4e96332cb6d123af65
Nils Rohe <nilsrohe@freenet.de>
2019-03-20 17:27:06 +0100

    created new component user

---
+++ src/components/User.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class User extends Component {
+
+  render() {
+    return (
+      <input type="button" value="Sign-in"></input>
+    )
+  }
+
+}
+
+export default User;
